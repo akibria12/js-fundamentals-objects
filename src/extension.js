1fa9b154ec4aa8ab977d1180b3ef217972006811
Mustafa <mustafamuhith@gmail.com>
2022-09-11 15:15:31 +0100

    4ans

--- src/extension.js
+++ src/extension.js
@@ -15,11 +15,12 @@ const book = {
   dimensions: '10x12x2'
 }
 
-// 1. Set this to the book name - using the book object
-const name = ''
+const isbn13 = '978-0132350884'
 
+// 1. Set this to the book name - using the book object
+const name = book.name
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn10
 
 // Do not modify this basket object directly
 const basket = {
@@ -39,11 +40,10 @@ const basket = {
 }
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
-
+const firstVoucherCode = basket.voucherCodes[0]
 // Do not edit this exported object
 module.exports = {
   name: name,
